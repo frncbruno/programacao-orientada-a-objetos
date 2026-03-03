ad5bc7c0e3e95bcee01d0c9604c907ba716fe566
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-03-03 10:19:47 -0300

    Create exercicio1.java

---
+++ exercicios/For & matrizes/exercicio1.java
@@ -0,0 +1,24 @@
+package pkg;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Scanner teclado = new Scanner(System.in);
+
+		int[] vetor = new int[5]; //int vetor[5];
+		vetor[0] = 5;
+		vetor[1] = 10;
+		vetor[2] = 15;
+		vetor[3] = 20;
+		vetor[4] = 25;
+		
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.println("Elemento "+i+" = "+vetor[i]);
+		}
+
+		teclado.close();
+	}
+}
