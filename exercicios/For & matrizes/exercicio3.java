c44a17a10d4f0748b84d865b3af18ea310d144f6
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-03-03 10:36:27 -0300

    Create exercicio3.java

---
+++ exercicios/For & matrizes/exercicio3.java
@@ -0,0 +1,27 @@
+package pkg;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Scanner teclado = new Scanner(System.in);
+		int[][] matriz = new int [2][2];
+		for (int i = 0; i < 2; i++) {
+			for(int j = 0; j <2; j++) {
+				System.out.println("Digite o elemento ["+i+"]["+j+"]:");
+				matriz[i][j] = teclado.nextInt();
+			}
+		}
+		
+		System.out.println("Matriz digitada:");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(matriz[i][j]);
+            }
+            System.out.println();
+         }
+		teclado.close();
+	}
+}
