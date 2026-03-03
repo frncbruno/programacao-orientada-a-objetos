b7b755ae8a23e76b6acd4b87a18175692478c8f6
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-03-03 08:27:56 -0300

    Create exercicio4.java

---
+++ exercicios/Lista2/exercicio4.java
@@ -0,0 +1,15 @@
+package pkg;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		int soma = 0;
+
+		for (int i = 1; i <= 100; i++) {
+			soma += i;   
+		}
+
+		System.out.println("A soma de 1 até 100 é: " + soma);
+	}
+}
