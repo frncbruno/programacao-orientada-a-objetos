ce9455802c4c19745e9bdecbe0332f52cc9563f7
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-03-03 08:25:22 -0300

    Add multiplication table for 7 in exercicio3.java

---
+++ exercicios/Lista2/exercicio3.java
@@ -0,0 +1,12 @@
+package pkg;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i <= 10; i++) {
+			System.out.println("7 x " + i + " = " + (7 * i));
+		}
+		
+	}
+}
