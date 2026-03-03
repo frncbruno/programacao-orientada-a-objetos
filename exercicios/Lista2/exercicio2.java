1eb31aa1255069debcf477cc6447c6963584ae3e
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-03-03 08:22:50 -0300

    Create exercicio2.java

---
+++ exercicios/Lista2/exercicio2.java
@@ -0,0 +1,15 @@
+package pkg;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		for(int i = 2; i < 21; i++) {
+			if(i % 2 == 0) { 
+				System.out.println("I: "+i);
+			}
+		}
+	}
+}
