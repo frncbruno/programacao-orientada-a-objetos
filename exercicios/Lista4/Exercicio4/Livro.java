cbe85ec33922844ea6878951ebe2cebdf87ac80e
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-03-10 14:33:04 -0300

    Update Livro.java

--- exercicios/Lista4/Exercicio4/Livro.java
+++ exercicios/Lista4/Exercicio4/Livro.java
@@ -1,7 +1,7 @@
 package projeto;
 
-public class Computador {
+public class Livro {
 	public String titulo;
-  public String autor; 
-  public int anoPublicacao;
+  	public String autor; 
+  	public int anoPublicacao;
 }
