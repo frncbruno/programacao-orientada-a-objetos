b936fd54357574ee1ac25be4b540c659cde46d0f
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-03-17 10:32:42 -0300

    Create Carro.java

---
+++ exercicios/Lista5/Exercicio2/Carro.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Carro {
+	public String marca;
+	public String modelo;
+	
+	public Carro(String marcaCarro, String modeloCarro) { 
+		System.out.println("-- Estou no construtor! --");
+		marca = marcaCarro;
+		modelo = modeloCarro;
+	}
+	
+	public Carro(String marca) {
+		System.out.println("-- Estou no outro construtor! --");
+		this.marca = marca;	
+	}
+	
+	public void atribuirValores(String ma, String mo) {
+		marca = ma;
+		modelo = mo;
+	}
+	
+	public String retornaMarca() {
+		return marca;
+	}
+	
+	public String retornaModelo() {
+		return modelo;
+	}
+}
