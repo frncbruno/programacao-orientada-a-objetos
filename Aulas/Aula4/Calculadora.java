0d292b5d6a16abbc301247212de8fea470c65282
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-03-17 08:28:56 -0300

    Create Calculadora.java

---
+++ Aulas/Aula4/Calculadora.java
@@ -0,0 +1,13 @@
+package pkg;
+
+public class Calculadora {
+	public double a;
+	public double b;
+	
+	public double somar() {
+//		double resultado;
+//		resultado = a + b;
+//		return resultado;
+		return a + b;
+	} 
+}
