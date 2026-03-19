0d52164e95330d5fbfd71cf9c8234794506f4dcf
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-03-19 08:38:51 -0300

    Create Main.java

---
+++ exercicios/Lista6/Exercicio6/Main.java
@@ -0,0 +1,11 @@
+package pkg;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Aluno a1 = new Aluno("João", "12345");
+        Aluno a2 = new Aluno("10/03/2000");
+        Aluno a3 = new Aluno("Maria", "15/08/1999", 2018);
+    }
+}
