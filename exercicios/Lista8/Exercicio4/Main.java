818790f4d64d6e8b0a5807ae47a1934898e4ca59
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-03-31 10:06:42 -0300

    Create Main.java

---
+++ exercicios/Lista8/Exercicio4/Main.java
@@ -0,0 +1,15 @@
+package pkg;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Retangulo r = new Retangulo(5, 10);
+		
+		r.calcularArea();
+		r.calcularPerimetro();
+		r.calcularDiagonal();
+		r.ehQuadrado();
+		
+	}
+
+}
