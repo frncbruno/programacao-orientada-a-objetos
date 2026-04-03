13b240a77424e1ff18d0c5b5c0ef59f47c88bbbd
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-04-03 13:07:47 -0300

    Create Pedido.java

---
+++ exercicios/Lista8/Exercicio7/Pedido.java
@@ -0,0 +1,33 @@
+package pkg;
+
+public class Pedido {
+
+    public int numero;
+    public String produto;
+    public double preco;
+    public int quantidade;
+
+
+    public Pedido(int numero, String produto, double preco, int quantidade) {
+        this.numero = numero;
+        this.produto = produto;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public double calcularSubtotal() {
+        return preco * quantidade;
+    }
+
+    public double calcularDesconto(double percentual) {
+        return calcularSubtotal() * (percentual / 100);
+    }
+
+    public double calcularTotal(double percentualDesconto) {
+        return calcularSubtotal() - calcularDesconto(percentualDesconto);
+    }
+
+    public String resumoPedido() {
+        return "Pedido #" + numero + ": " + produto + " - Preço: R$" + preco + ", Quantidade: " + quantidade;
+    }
+}
