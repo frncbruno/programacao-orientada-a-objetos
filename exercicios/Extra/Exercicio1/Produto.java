69921b9c72592ed5dd1bdd8d70db5e9031a8e22a
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-04-21 22:05:20 -0300

    Update Produto.java

--- exercicios/Extra/Exercicio1/Produto.java
+++ exercicios/Extra/Exercicio1/Produto.java
@@ -1,47 +1,50 @@
-class Produto {
+public class Produto {
+
     private String nome;
     private double preco;
     private int quantidadeEstoque;
 
-    // Construtor 1
+    // Construtor só com nome
     public Produto(String nome) {
         this.nome = nome;
         this.preco = 0;
         this.quantidadeEstoque = 0;
     }
 
-    // Construtor 2
+    // Construtor com nome e preço
     public Produto(String nome, double preco) {
         this.nome = nome;
         this.preco = preco;
         this.quantidadeEstoque = 0;
     }
 
-    // GET e SET
+    // Getters
     public String getNome() {
         return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public double getPreco() {
         return preco;
     }
 
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
-
     public int getQuantidadeEstoque() {
         return quantidadeEstoque;
+    }
+
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
     }
 
     public void setQuantidadeEstoque(int quantidadeEstoque) {
         this.quantidadeEstoque = quantidadeEstoque;
     }
 
+    // Método exibir
     public void exibirInformacoes() {
         System.out.println("Nome: " + nome);
         System.out.println("Preço: " + preco);
