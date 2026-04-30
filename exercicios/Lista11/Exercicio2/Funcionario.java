14f615c4975ad28697b08ab61949955a6a5f4165
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-04-30 20:36:11 -0300

    Create Funcionario.java

---
+++ exercicios/Lista11/Exercicio2/Funcionario.java
@@ -0,0 +1,11 @@
+public class Funcionario extends Pessoa {
+ 
+    public Funcionario(String nome) {
+        super(nome);
+    }
+ 
+    public void trabalhar() {
+        System.out.println("O funcionário está trabalhando.");
+    }
+}
+ 
