c7e31ddc357680a9189ad1db64ea7392d08b66d5
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-04-30 20:34:09 -0300

    Create Figura.java

---
+++ exercicios/Lista11/Exercicio1/Figura.java
@@ -0,0 +1,21 @@
+public class Figura {
+ 
+    protected String cor;
+    protected boolean preenchido;
+ 
+    public String getCor() {
+        return cor;
+    }
+ 
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+ 
+    public boolean isPreenchido() {
+        return preenchido;
+    }
+ 
+    public void setPreenchido(boolean preenchido) {
+        this.preenchido = preenchido;
+    }
+}
