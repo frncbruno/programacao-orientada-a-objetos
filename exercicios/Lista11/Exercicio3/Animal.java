4e6b82d3538e12cb6074a6d7a6a339759576a0ca
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-04-30 20:37:23 -0300

    Create Animal.java

---
+++ exercicios/Lista11/Exercicio3/Animal.java
@@ -0,0 +1,17 @@
+public class Animal {
+ 
+    protected String tipo;
+ 
+    public String getTipo() {
+        return tipo;
+    }
+ 
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+ 
+    public void andar() {
+        System.out.println("Todos andam, mas o modo é variado.");
+    }
+}
+ 
