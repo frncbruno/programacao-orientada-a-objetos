f518509f88881fd5a60a10b17aa41cef9b5401fb
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-04-30 20:37:54 -0300

    Create Pessoa.java

---
+++ exercicios/Lista11/Exercicio3/Pessoa.java
@@ -0,0 +1,7 @@
+public class Pessoa extends SerHumano {
+
+    // Pessoa herda de SerHumano (nome, idade, falar)
+    // SerHumano herda de Animal (tipo, andar)
+    // Pessoa tem acesso a tudo automaticamente
+
+}
