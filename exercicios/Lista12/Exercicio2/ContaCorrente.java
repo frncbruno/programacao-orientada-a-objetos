9f76ed05fcd2c6b5f2ac54ab01cbf100acc12767
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-05-05 20:22:18 -0300

    Create ContaCorrente.java

---
+++ exercicios/Lista12/Exercicio2/ContaCorrente.java
@@ -0,0 +1,9 @@
+package pkg;
+
+public class ContaCorrente extends ContaBancaria {
+    @Override
+    public void calcularSaldo() {
+        System.out.println("Calculando saldo da conta corrente...");
+    }
+    
+}
