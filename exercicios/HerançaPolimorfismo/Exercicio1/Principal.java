a886244e517fa0707a2cad4aff9d4da904e9c13f
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-05-05 09:21:04 -0300

    Create Principal.java

---
+++ exercicios/HerançaPolimorfismo/Exercicio1/Principal.java
@@ -0,0 +1,24 @@
+package pkg;
+
+import java.util.Scanner;
+
+class Principal {
+
+	public static void main(String[] args) {
+		Pessoa pessoa;
+		System.out.println("Digite 1 para pessoa");
+		System.out.println("Digite 2 para programador");
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		int op = teclado.nextInt();
+		if(op == 1) {
+			pessoa = new Pessoa();
+			pessoa.trabalhar();
+		} else if (op == 2) {
+			pessoa = new Programador();
+			pessoa.trabalhar();
+		}
+	}
+
+}
