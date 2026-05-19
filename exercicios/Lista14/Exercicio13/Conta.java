0c74971a8780abfc69bb59486de3dd72a89b4b53
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-05-19 19:04:47 -0300

    Create Conta.java

---
+++ exercicios/Lista14/Exercicio13/Conta.java
@@ -0,0 +1,44 @@
+public class Conta {
+    private int numero;
+    private String titular;
+    private double saldo;
+
+    public Conta(int numero, String titular, double saldo) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+        }
+    }
+
+    public void sacar(double valor) {
+        if (valor > 0 && valor <= saldo) {
+            saldo -= valor;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Conta{" +
+                "numero=" + numero +
+                ", titular='" + titular + '\'' +
+                ", saldo=" + saldo +
+                '}';
+    }
+}
