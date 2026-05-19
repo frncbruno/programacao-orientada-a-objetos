8dc32ce6cd1e65abfc7e33a0a30bb96d1dec16cd
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-05-19 18:54:07 -0300

    Create Reserva.java

---
+++ exercicios/Lista14/Exercicio8/Reserva.java
@@ -0,0 +1,11 @@
+package pkg;
+
+public class Reserva {
+    public void adicionar() {
+        System.out.println("Reserva adicionada.");
+    }
+
+    public void cancelar() {
+        System.out.println("Reserva cancelada.");
+    }
+}
