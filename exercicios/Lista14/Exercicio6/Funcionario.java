0578f1a4853cd7dcb81a4d324a84d760cdda5836
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-05-19 10:17:23 -0300

    Create Funcionario.java

---
+++ exercicios/Lista14/Exercicio6/Funcionario.java
@@ -0,0 +1,22 @@
+public class Funcionario {
+    protected String nome;
+    protected double salario;
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public void aumentarSalario(double porcentagem) {
+        salario += salario * (porcentagem / 100);
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "nome='" + nome + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+    
+}
