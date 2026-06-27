37f2c9b5f21f1ba8dca0f1f6cd2b8ecc347cba90
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-06-27 15:47:25 -0300

    Update Servidor.java

--- Trabalho N3/Servidor.java
+++ Trabalho N3/Servidor.java
@@ -8,30 +8,24 @@ public class Servidor {
 
     public static void main(String[] args) {
         try {
-            // Criacao do socket servidor e definicao da porta (1234)
+            // esse 1234 seria a porta do IP ficticia de acesso
             ServerSocket servidor = new ServerSocket(1234);
             System.out.println("Servidor iniciado. Aguardando conexoes...");
 
             while (true) {
-                /* Servidor aguarda uma conexao de um cliente
-                   O servidor bloqueia nesta linha enquanto um cliente nao chega
-                   Quando um cliente se conecta, o servidor recebe uma referencia do
-                   socket do cliente que se conectou (retorno do metodo) */
                 Socket cliente = servidor.accept();
                 System.out.println("Cliente conectado!");
 
-                // Extraio o fluxo de saida e entrada de objetos do socket
                 ObjectOutputStream out = new ObjectOutputStream(cliente.getOutputStream());
                 ObjectInputStream in = new ObjectInputStream(cliente.getInputStream());
 
                 Veiculo veiculo;
                 Resposta res;
 
-                // Servidor recebe 1 objeto do tipo Veiculo do cliente
                 veiculo = (Veiculo) in.readObject();
                 System.out.println("Veiculo recebido: " + veiculo);
 
-                // Servidor tenta cadastrar o veiculo e prepara a resposta
+                // cadastra o veiculo e envia a resposta
                 try {
                     ArquivoVeiculo arquivo = new ArquivoVeiculo("veiculos");
                     List<Veiculo> listaVeiculos = arquivo.lerLista();
@@ -42,7 +36,6 @@ public class Servidor {
                     res = new Resposta("Erro ao cadastrar o veiculo.");
                 }
 
-                // Servidor envia o objeto Resposta para o cliente
                 out.writeObject(res);
 
                 cliente.close();
