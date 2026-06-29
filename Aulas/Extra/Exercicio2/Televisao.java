e2dc1acbce6c70bd996dc3ccd7fb2014a764474e
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-06-29 16:39:36 -0300

    Create Televisao.java

---
+++ Aulas/Extra/Exercicio2/Televisao.java
@@ -0,0 +1,12 @@
+public class Televisao implements Controlavel {
+
+    public void ligar() {
+        System.out.println("Televisao ligada.");
+        System.out.println("Canal inicial: 1");
+    }
+
+    public void desligar() {
+        System.out.println("Televisao desligada.");
+    }
+
+}
