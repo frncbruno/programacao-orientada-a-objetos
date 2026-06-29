a26c5d6cdb856fbff393f05456599e8303fe1ec7
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-06-29 16:44:08 -0300

    Create Livro.java

---
+++ Aulas/EstudoProva/Exercicio2/Livro.java
@@ -0,0 +1,24 @@
+public class Livro {
+
+    // ATRIBUTOS
+    private String nome;
+    private String autor;
+
+    // CONSTRUTOR
+    public Livro(String nome, String autor) {
+
+        this.nome = nome;
+        this.autor = autor;
+
+    }
+
+    // GETTERS
+    public String getNome() {
+        return nome;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+}
